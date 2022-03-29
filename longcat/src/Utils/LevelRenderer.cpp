8fe5ced84532f472157e4d6017ee44ce34ab6469
jaguile6 <75176528+jaguile6@users.noreply.github.com>
2022-03-29 23:37:28 +0200

    fix tabs

--- longcat/src/Utils/LevelRenderer.cpp
+++ longcat/src/Utils/LevelRenderer.cpp
@@ -100,7 +100,7 @@ void LevelRenderer::render(Game &game)
     renderStuck(70,0,game);
     if (game.getGameMode() == GameMode::Random)
     {
-		CatChars::print(70, 10, true, " Seed:   ");	
+        CatChars::print(70, 10, true, " Seed:   ");	
         renderRandData(64,20,game);
     }
 }
